e300d6447d7fdf0ad531267ac51ddc2b64747318
Kishn0109 <raikaran159@gmail.com>
2024-05-05 00:00:53 +0530

    feat: Integrate filters wit query params

--- src/Filter/index.jsx
+++ src/Filter/index.jsx
@@ -1,23 +1,42 @@
 import React, { useState } from "react";
 import Filter from "./Filter";
 import { FormControl, InputLabel, OutlinedInput } from "@mui/material";
+import { useSearchParams } from "react-router-dom";
 const experiences = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 const remoteOptions = ["In-office", "Hybrid", "Remote"];
 const minBaseSalaryOption = [...new Array(7)].map(
   (_, index) => index + 10 + "L"
 );
 const roleOptions = ["Backend", "Frontend", "Flutter"];
+export function extractExistingParams(searchParams) {
+  const entries = Array.from(searchParams.entries());
+  return entries.reduce(
+    (acc, a) => ((acc[a[0]] = acc[a[0]] || []).push(a[1]), acc),
+    {}
+  );
+}
 export default function Filters() {
-  const [roles, setRoles] = useState([]);
-  const [remote, setRemote] = useState("");
-  const [experience, setExperience] = useState("");
-  const [minBaseSalary, setMinBaseSalary] = useState("");
-  const [companyName, setCompanyName] = useState("");
+  let [searchParams, setSearchParams] = useSearchParams();
+  const params = extractExistingParams(searchParams);
+  const [roles, setRoles] = useState(params.roles || []);
+  const [remote, setRemote] = useState(params.remote || "");
+  const [experience, setExperience] = useState(params.experience || "");
+  const [minBaseSalary, setMinBaseSalary] = useState(
+    params.minBaseSalary || ""
+  );
+  const [companyName, setCompanyName] = useState(params.companyName || "");
   const onChange = (event) => {
     const {
       target: { value },
     } = event;
     setCompanyName(value);
+    setParam("companyName", value);
+  };
+  const setParam = (key, value) => {
+    setSearchParams({
+      ...params,
+      [key]: value,
+    });
   };
   return (
     <div>
@@ -25,7 +44,10 @@ export default function Filters() {
         options={roleOptions}
         label="Roles"
         value={roles}
-        setValue={setRoles}
+        setValue={(value) => {
+          setRoles(value);
+          setParam("roles", value);
+        }}
         multiple
         placeholder="Roles"
       />
@@ -33,21 +55,30 @@ export default function Filters() {
         options={remoteOptions}
         label="Remote"
         value={remote}
-        setValue={setRemote}
+        setValue={(value) => {
+          setRemote(value);
+          setParam("remote", value);
+        }}
         placeholder="Remote"
       />
       <Filter
         options={experiences}
         label="Experience"
         value={experience}
-        setValue={setExperience}
+        setValue={(value) => {
+          setExperience(value);
+          setParam("experience", value);
+        }}
         placeholder="Experience"
       />
       <Filter
         options={minBaseSalaryOption}
         label="Min Base Pay"
         value={minBaseSalary}
-        setValue={setMinBaseSalary}
+        setValue={(value) => {
+          setMinBaseSalary(value);
+          setParam("minBaseSalary", value);
+        }}
         placeholder="Min Base Pay"
       />
       <FormControl sx={{ m: 1, width: 150 }}>
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,17 @@ import App from "./App.jsx";
 import "./index.css";
 import { store } from "./store";
 import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+  // </React.StrictMode>
 );
